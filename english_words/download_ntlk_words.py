c83977f21c247ce4170b22cfc348cd79e42c7de7
Elad Nachum <eladnach@amdocs.com>
2017-12-02 22:54:07 +0200

    commit

---
+++ english_words/download_ntlk_words.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import nltk
+#from nltk.corpus import words
+
+nltk.download()
+
+#nltk_word_list = words.words()
+# prints 236736
+#print len(word_list)
---
+++ english_words/words.py
@@ -0,0 +1,4 @@
+from nltk.corpus import words
+word_list = words.words()
+# prints 236736
+print len(word_list)
